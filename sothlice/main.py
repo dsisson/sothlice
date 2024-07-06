b7a6cfed4d9960460335c9b7de91c538126f7a83
dsisson <philosophe@gmail.com>
2024-07-06 14:04:49 -0400

    refactor write to db
    README.md
    + added content
    
    sothlice/main.py
    + created get_tickets_from_jira() and abstracted code from main()
    + process_tickets() and abstracted code from main()
    + get_ticket_ids_from_db() and abstracted code from main()
    + create_insert_query() and abstracted code from main()
    + refactored main()
    + changed db mode from 'update' to 'insert'

--- sothlice/main.py
+++ sothlice/main.py
@@ -47,45 +47,49 @@ def get_db_mode():
        :return mode: str, database mode
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default='update')
+    parser.add_argument('--mode', type=str, default='insert')
     args = parser.parse_args()
     print(f"args: {args}")
     return args.mode
 
 
-def main():
-    # start logging
-    logger = setup_logging('sothlice', 'sothlice.log')
-    logger.info('Starting ')
+def get_tickets_from_jira(jira, logger):
+    """
+        Get all the non-completed tickets from Jira using the Jira API.
 
-    # ----------------- JIRA -----------------
-    # connect to Jira
-    jira = Jira(
-        url='https://philosophe.atlassian.net',
-        username='philosophe@gmail.com',
-        password=get_token(),
-        cloud=True)
-
-    logger.info(f"Jira response: {jira}")
-    logger.info(f"dir.api_version: {jira.api_version}")
-    #logger.info(f"dir(jira): \n{plog(dir(jira))}")
-    logger.info(f"jira.__dict__: \n{plog(jira.__dict__)}")
-
-    # get the tickets
+       :param jira: Jira, the Jira API object
+       :return issues: dict, the Jira tickets
+    """
     jql_query = 'project = "Sample Scrum Project" ' \
                   'AND status NOT IN (Closed, Resolved) ' \
                   'ORDER BY issuekey'
+
+    # get the tickets
     issues = jira.jql(jql_query)
     # logger.info(f"issues: \n{plog(issues)}")
+    return issues
 
-    # ----------------- massage data -----------------
-    # create the data container for the extracted ticket information
+
+def process_tickets(issues, logger):
+    """
+        Process the Jira tickets to extract the desired information.
+
+       :param issues: dict, the Jira tickets
+       :return ticket_data: list, the extracted ticket information
+    """
+    # create data container for the extracted ticket information
     ticket_data = []
+    # create container for just the ticket IDs
+    ticket_ids = []
 
     # extract the desired ticket information
     for issue in issues['issues']:
+        id = issue['key']
+        ticket_ids.append(id)
+
+        # build the data
         ticket = {
-            'key': issue['key'],
+            'key': id,
             'type': issue['fields']['issuetype']['name'],
             'status': issue['fields']['status']['name'],
             'summary': issue['fields']['summary'],
@@ -102,91 +106,142 @@ def main():
         ticket_data.append(ticket)
 
     logger.info(f"ticket_data: \n{plog(ticket_data)}")
+    return ticket_data, ticket_ids
+
+
+def get_ticket_ids_from_db(connection, jira_ids, logger):
+    """
+        Get the ticket IDs from the database, compare with the ids for
+        our Jira tickets (because we only want to handle new ones), and
+        return the list of IDs we want to keep.
+
+        NOTE that we are only ADDING rows, and NOT updating rows with changes.
+        Also note that we need to convert the db fdata into a form we can use.
+
+        :param connection: psycopg2.connection, the database connection
+        :param jira_ids: list, the Jira ticket IDs
+        :param logger: logging.Logger, the logger
+        :return ticket_ids: list, the ticket IDs
+    """
+    cursor = connection.cursor()
+    query = "SELECT jkey FROM jira;"
+    cursor.execute(query)
+    raw_existing_ids = cursor.fetchall()
+    cursor.close()
+
+    # compare the ticket IDs from the DB with the Jira ticket IDs
+    existing_ids = [id[0] for id in raw_existing_ids]
+    logger.info(f"\nexisting_ids: {existing_ids}")
+
+    # get the ids to delete from the Jira data
+    delete_ids = [id for id in jira_ids if id in existing_ids]
+
+    return delete_ids
+
+
+def create_insert_query(ticket, logger):
+    """
+        Create the insert query for the database.
+
+       :param ticket: dict, the ticket data
+       :return query: str, the insert query
+    """
+    logger.info(f"\nthis ticket: \n{ticket}")
+
+    # simplify the insert
+    jkey = ticket['key']
+    jtype = ticket['type']
+    status = ticket['status']
+    summary = ticket['summary']
+    description = ticket['description']
+    created = ticket['created']
+    updated = ticket['updated']
+    assignee = ticket['assignee']
+
+    values = (jkey, jtype, status, summary,
+              description, created, updated, assignee)
+    sql = "INSERT INTO jira (jkey, jtype, status, summary, description, created, updated, assignee) " \
+          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING jkey;"
+    return sql, values
+
+
+def main():
+    # start logging
+    logger = setup_logging('sothlice', 'sothlice.log')
+    logger.info('Starting ')
+
+    # ----------------- JIRA -----------------
+    # Step 1: connect to Jira
+    jira = Jira(
+        url='https://philosophe.atlassian.net',
+        username='philosophe@gmail.com',
+        password=get_token(),
+        cloud=True)
+
+    logger.info(f"dir.api_version: {jira.api_version}")
+    # logger.info(f"jira.__dict__: \n{plog(jira.__dict__)}")
+
+    # Step 2: get the tickets from Jira
+    raw_issues = get_tickets_from_jira(jira, logger)
+
+    # ----------------- massage the tickets data -----------------
+    # Step 3: extract and enrich the tickets data
+    updated_tickets, jira_ids = process_tickets(raw_issues, logger)
 
     # ----------------- db -----------------
-    # identify the database mode
+    # Step 4: identify the database mode (because we may do different things)
     mode = get_db_mode()
 
-    # connect to the database
+    # Step 5: connect to the postgres database
     connection = psycopg2.connect(
         host='localhost',
         database='qdata',
         user='derek')
 
-    # Open a cursor to perform database operations
-    cursor = connection.cursor()
-    logger.info(f"\ncursor: {cursor}")
+    # Step 6: get the ids that are in the Jira data AND the db
+    delete_ids = get_ticket_ids_from_db(connection, jira_ids, logger)
 
-    # get existing IDs because are updating NEW tickets
-    query = "SELECT jkey FROM jira;"
-    cursor.execute(query)
-    connection.commit()
-    raw_existing_ids = cursor.fetchall()
-
-    # compare the ticket IDs from the DB with the Jira ticket IDs
-    existing_ids = [id[0] for id in raw_existing_ids]
-    logger.info(f"\nexisting_ids: {existing_ids}")
-    jira_ids = [t['key'] for t in ticket_data]
-
-    # get the ids to delete from the Jira data
-    delete_ids = [id for id in jira_ids if id in existing_ids]
-
-    # update our Jira tickets data model to be only NEW tickets (wrt the db)
-    new_jira_tickets = [t for t in ticket_data if t['key'] not in delete_ids]
+    # Step 7: update our Jira tickets data model to be only NEW tickets (wrt the db)
+    new_jira_tickets = [t for t in updated_tickets if t['key'] not in delete_ids]
 
     # ##################################################
     # # take specific db actions based on the CLI args #
     # ##################################################
-    if mode == 'update':
-
-        cursor.execute("SELECT * from jira;")
-        connection.commit()
-        logger.info(f"\ncursor.query: {cursor.query}")
-        logger.info(f"\ncursor.fetchall(): {cursor.fetchall()}")
+    if mode == 'insert':
+        # ##################################################
+        # # insert records that do not exists in DB        #
+        # ##################################################
 
         logger.info(f"\n---> will insert {len(new_jira_tickets)} tickets")
         # insert the data
         for ticket in new_jira_tickets:
-            logger.info(f"\nthis ticket: \n{ticket}")
 
-            # simplify the insert
-            jkey = ticket['key']
-            jtype = ticket['type']
-            status = ticket['status']
-            summary = ticket['summary']
-            description = ticket['description']
-            created = ticket['created']
-            updated = ticket['updated']
-            assignee = ticket['assignee']
-
-            values = (jkey, jtype, status, summary,
-                      description, created, updated, assignee)
-            sql = "INSERT INTO jira (jkey, jtype, status, summary, description, created, updated, assignee) " \
-                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING jkey;"
+            # Step 8a: assemble the query and the values to insert
+            sql, values = create_insert_query(ticket, logger)
             try:
+                cursor = connection.cursor()
+                # Step 8b: for each record to be inserted, insert it
                 cursor.execute(sql, values)
                 connection.commit()
 
-                logger.info(f"\ncursor.query: {cursor.query}")
-                logger.info(f"\ncursor.description: {cursor.description}")
                 logger.info(f"\ncursor.statusmessage: {cursor.statusmessage}")
                 logger.info(f"\ncursor.rowcount: {cursor.rowcount}")
                 res = cursor.fetchone()
                 logger.info(f"\nthis res: {res}")
+                cursor.close()
             except psycopg2.errors.UniqueViolation as e:
                 logger.error(f"\nerror: {e}")
 
+    # Step 9: get the updated data from the db as a check
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from jira;")
+    result = cursor.fetchall()
+    for i, row in enumerate(result):
+        logger.info(f"row {i}: {row}")
 
-        cursor.execute("SELECT * from jira;")
-        result = cursor.fetchall()
-        for i, row in enumerate(result):
-            logger.info(f"row {i}: {row}")
-
-
-    # clean up
+    # Step 10: clean up
     cursor.close()
     connection.close()
-
 
 
 if __name__ == '__main__':
